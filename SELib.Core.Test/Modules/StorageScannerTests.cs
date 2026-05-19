144a5b73a139ab62a73cea62b6195576388f4077
PaulusDeGross <maerzpaul06@gmail.com>
2026-05-19 13:35:23 +0200

    Initial Library and Test Setup

--- SELib.Core.Test/Modules/StorageScannerTests.cs
+++ SELib.Core.Test/Modules/StorageScannerTests.cs
@@ -1,11 +1,41 @@
+using SELib.Core.Modules;
+
 namespace SELib.Core.Test.Modules
 {
-    public class StorageScannerTests
+    public class StorageScannerTests : IDisposable
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly string _tempDir;
 
+        public StorageScannerTests()
+        {
+            _tempDir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(_tempDir);
+
+            byte[] largeData = new byte[1024 * 1024];
+            byte[] smallData = new byte[1024];
+
+            File.WriteAllBytes(Path.Combine(_tempDir, "large_file.dat"), largeData);
+            File.WriteAllBytes(Path.Combine(_tempDir, "small_file.dat"), smallData);
+        }
+
+        [Fact]
+        public void GetFilesAboveSize_FiltersCorrectly()
+        {
+            // Act: Filter for files larger than 500 KB
+            long minSize = 500 * 1024;
+            var results = StorageScanner.GetFilesAboveSize(_tempDir, minSize).ToList();
+
+            // Assert
+            Assert.Single(results); // Should only find one file
+            Assert.EndsWith("large_file.dat", results[0].Path);
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(_tempDir))
+            {
+                Directory.Delete(_tempDir, true);
+            }
         }
     }
 }
--- SELib.Core/Modules/StorageScanner.cs
+++ SELib.Core/Modules/StorageScanner.cs
@@ -1,7 +1,40 @@
-﻿namespace SELib.Core.Modules
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SELib.Core.Modules
 {
+    public record FileItem(string Path, long SizeBytes);
+
     public class StorageScanner
     {
+        public static IEnumerable<FileItem> GetFilesAboveSize(string rootPath, long minSizeBytes)
+        {
+            var dirInfo = new DirectoryInfo(rootPath);
+            var opts = new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true,
+                ReturnSpecialDirectories = true,
+            };
 
+            IEnumerable<FileInfo> files;
+            try
+            {
+                files = dirInfo.EnumerateFiles("*.*", opts);
+            }
+            catch (Exception)
+            {
+                yield break;    // If root directory is denied, yield nothing
+            }
+
+            foreach (var file in files)
+            {
+                if (file.Length >= minSizeBytes)
+                {
+                    yield return new FileItem(file.FullName, file.Length);
+                }
+            }
+        }
     }
 }
